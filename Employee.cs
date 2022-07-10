55362b94a798aefab651a6a7e59bf4f7cce9bd68
iradakhf <iradakhf@code.edu.az>
2022-07-10 15:33:05 +0400

    add all

---
+++ Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class Employee
+    {
+        public int Hours { get; set; }
+        public int Salary { get; set; }
+        public int GetInfo(int salary, int hours)
+        {
+            this.Hours = hours;
+            this.Salary = salary;
+            return salary;
+        }
+        public int AddSalary()
+        { 
+            if (Salary < 5200)
+            {
+                Salary += 100;
+            }
+            return Salary;
+        }
+        public int AddWork()
+        {
+            if (Hours > 5)
+            {
+                Salary += 5;
+            }
+            return Salary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ConsoleApp25
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            Employee employee = new Employee();
+            employee.GetInfo(7000, 7);
+            employee.AddSalary();
+            employee.AddWork();
+            Console.WriteLine(employee.Salary);
+        }
+    }
+}
